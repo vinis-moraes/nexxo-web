11051f3049eb3fb311ec6ae8b1535eae3af64e11
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2025-12-04 20:43:07 -0300

    Update icons and improve viewport settings
    Replaced app icons with new versions and updated index.html viewport meta tag to restrict scaling and improve widget interaction. Also updated service worker and bootstrap files to reference new assets.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2892795234"
+    serviceWorkerVersion: "1967918702"
   }
 });
